134c2af918942edec4a29d7ffe09932040c002d7
bemri <emribali@gmail.com>
2019-01-06 19:43:36 +0100

    Update MainActivity.java

--- app/src/main/java/emribalazs/hu/etc/MainActivity.java
+++ app/src/main/java/emribalazs/hu/etc/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
     }
